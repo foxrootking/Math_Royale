e10c929b8c1f4565935b2b69c5d6b857413f00cf
SudoCrypted <vecchifrancesco2007@gmail.com>
2022-08-08 11:01:16 +0200

    Flipping Algorithm - Part done

--- Assets/scripts/Game/Mono/KeyGrid.cs
+++ Assets/scripts/Game/Mono/KeyGrid.cs
@@ -14,7 +14,10 @@ public class KeyGrid : MonoBehaviour
     public GameObject GenController;
 
     string[] chars_index = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "-", "0", "="};
+    int[] focal_sector = {9,10,8};
+    int focal_sector_indexer = 0;
     int n = 0;
+    int sub_val = 0;
 
     void Start()
     {
@@ -23,6 +26,7 @@ public class KeyGrid : MonoBehaviour
 
     void GenerateGrid()
     {
+
         for (int x = 0; x < width; x++) //Loops over width
         {
             for (int y = 0; y < height; y++) //Loops over height
@@ -30,7 +34,23 @@ public class KeyGrid : MonoBehaviour
                 var spawnedTile = Instantiate(button,new Vector3(x * 290 + spawnOffsetX,y * 230 + spawnOffsetY), Quaternion.identity);
                 spawnedTile.transform.SetParent(GenController.transform);
 
-                (spawnedTile.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>()).text = $"{chars_index[n]}";
+                if (y >= 4)
+                {
+                    focal_sector_indexer++;
+                }
+                
+                if (y >= 8)
+                {
+                    focal_sector_indexer++;
+                }
+
+                (spawnedTile.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>()).text = $"{chars_index[focal_sector[focal_sector_indexer] + sub_val]}";
+
+                if (sub_val <= -12)
+                {
+                    sub_val = 0;
+                }
+                sub_val -= 3;
                 (spawnedTile.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>()).fontSize = 200;
                 n++;
             }
