390b5ace499ad938a0ef39c3e394e030175de63c
0xGassin <vecchifrancesco2007@gmail.com>
2022-08-10 17:07:40 +0200

    Keyboard Logic Error Fix
    The Indexing Algorithm Used To Determine The Value Of Any Given Key Has Been Patched And Tailored To This Specific 3 By 4 Scenario.

--- Assets/scripts/Game/Mono/KeyGrid.cs
+++ Assets/scripts/Game/Mono/KeyGrid.cs
@@ -14,7 +14,7 @@ public class KeyGrid : MonoBehaviour
     public GameObject GenController;
 
     string[] chars_index = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "-", "0", "="};
-    int[] focal_sector = {9,10,8};
+    int[] focal_sector = {10,11,9};
     int focal_sector_indexer = 0;
     int n = 0;
     int sub_val = 0;
@@ -34,19 +34,19 @@ public class KeyGrid : MonoBehaviour
                 var spawnedTile = Instantiate(button,new Vector3(x * 290 + spawnOffsetX,y * 230 + spawnOffsetY), Quaternion.identity);
                 spawnedTile.transform.SetParent(GenController.transform);
 
-                if (y >= 4)
+                if (n == 3)
                 {
                     focal_sector_indexer++;
                 }
                 
-                if (y >= 8)
+                if (n == 7)
                 {
                     focal_sector_indexer++;
                 }
 
                 (spawnedTile.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>()).text = $"{chars_index[focal_sector[focal_sector_indexer] + sub_val]}";
 
-                if (sub_val <= -12)
+                if (sub_val == -12)
                 {
                     sub_val = 0;
                 }
